2e3a3964a9ce81b862a613a2bcc3deb3fbf64fe5
Aylar <116635687+AylOrAil@users.noreply.github.com>
2024-07-18 16:10:10 +0300

    Update simple_walker.py

--- rhex_ws/src/rhex_control/scripts/simple_walker.py
+++ rhex_ws/src/rhex_control/scripts/simple_walker.py
@@ -168,6 +168,9 @@ class SimpleWalker(Node):
             # WALK _ MODE 1: 8 Time Regions 
             if (self.state == 3):   
                 
+                self.cmd_kp = [3.75, 3.75, 3.75, 3.75, 3.75, 3.75]
+                self.cmd_kd = [0.35, 0.35, 0.35, 0.35, 0.35, 0.35]
+                
                 elapsed_time = ((time.time() - self.start_time)) * self.simulation_speedup
                 t_c = 1.6
                 t_s = 0.8
@@ -238,14 +241,14 @@ class SimpleWalker(Node):
                         self.cmd_vel[i] = (2* math.pi - phi_s)/(t_c - t_s)
                 
                 #### [0, phi_s/2] #### 
-                if (t_d + t_s + (t_c - t_s)/2) <= t < (t_d + t_s + 3* (t_c - t_s)/4):
+                if (t_d + t_c - t_s + (t_s)/2) <= t < (t_d + t_c - t_s + 3* (t_s)/4):
                     for i in [2, 4, 0]:
                         pos = self.currPos[i]
                         self.cmd_tau[i] = 0.0
                         self.cmd_pos[i] = (phi_s/4)
                         self.cmd_vel[i] = (phi_s/ t_s)
                         
-                elif 0 <= t < t_d or (t_d + t_s + 3* (t_c - t_s)/4) <= t < t_c:
+                elif 0 <= t < t_d or (t_d + t_c - t_s + 3* (t_s)/4) <= t < t_c:
                     for i in [2, 4, 0]:
                         pos = self.currPos[i]
                         self.cmd_tau[i] = 0.0
@@ -253,14 +256,14 @@ class SimpleWalker(Node):
                         self.cmd_vel[i] = (phi_s/ t_s)
                 
                 #### [phi_s/2, pi] #####
-                elif t_d <= t < (t_d + t_s/4):
+                elif t_d <= t < (t_d + (t_c -t_s)/4):
                     for i in [2, 4, 0]:
                         pos = self.currPos[i]
                         self.cmd_tau[i] = 0.0
                         self.cmd_pos[i] = (math.pi+phi_s/2)/2
                         self.cmd_vel[i] = (2* math.pi - phi_s)/(t_c - t_s)
                         
-                elif (t_d + t_s/4) <= t < (t_d + t_s/2):
+                elif (t_d + (t_c - t_s)/4) <= t < (t_d + (t_c - t_s)/2):
                     for i in [2, 4, 0]:
                         pos = self.currPos[i]
                         self.cmd_tau[i] = 0.0
@@ -268,14 +271,14 @@ class SimpleWalker(Node):
                         self.cmd_vel[i] = (2* math.pi - phi_s)/(t_c - t_s)
                 
                 #### [pi, -phi_s/2]
-                elif (t_d + t_s/2) <= t < (t_d + 3* t_s/4):
+                elif (t_d + (t_c - t_s)/2) <= t < (t_d + 3* (t_c - t_s)/4):
                     for i in [2, 4, 0]:
                         pos = self.currPos[i]
                         self.cmd_tau[i] = 0.0
                         self.cmd_pos[i] = (math.pi+(2*math.pi - phi_s/2))/2
                         self.cmd_vel[i] = (2* math.pi - phi_s)/(t_c - t_s)  
                 
-                elif (t_d+ 3* t_s/4) <= t < (t_d + t_s):
+                elif (t_d+ 3* (t_c - t_s)/4) <= t < (t_d + t_c - t_s):
                     for i in [2, 4, 0]:
                         pos = self.currPos[i]
                         self.cmd_tau[i] = 0.0
@@ -284,14 +287,14 @@ class SimpleWalker(Node):
                 
                 
                 #### [-phi_s/2, 0]
-                elif (t_d + t_s) <= t < (t_d + t_s + (t_c - t_s)/4):
+                elif (t_d + t_c - t_s) <= t < (t_d + t_c - t_s + (t_s)/4):
                     for i in [2, 4, 0]:
                         pos = self.currPos[i]
                         self.cmd_tau[i] = 0.0
                         self.cmd_pos[i] = -phi_s/4
                         self.cmd_vel[i] = (phi_s/ t_s)
                 
-                elif (t_d + t_s + (t_c - t_s)/4) <= t < (t_d + t_s + (t_c - t_s)/2):
+                elif (t_d + t_c - t_s + (t_s)/4) <= t < (t_d + t_c - t_s + (t_s)/2):
                     for i in [2, 4, 0]:
                         pos = self.currPos[i]
                         self.cmd_tau[i] = 0.0
@@ -303,7 +306,7 @@ class SimpleWalker(Node):
             if (self.state == 4):   
                 elapsed_time = ((time.time() - self.start_time)) * self.simulation_speedup
                 t_c = 2.0
-                t_s = 1.0 
+                t_s = 1.0 # assumption: t_s = t_c /2 
                 
                 self.cmd_kp = [4.75, 4.75, 4.75, 4.75, 4.75, 4.75]
                 self.cmd_kd = [0.35, 0.35, 0.35, 0.35, 0.35, 0.35]
